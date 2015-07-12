bc3477d19de76cf3df4945660030ca9ef3137a4a
johnnyc16 <johnnyc16@gmail.com>
2015-07-12 04:42:35 +0200

    añadido heroku

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
