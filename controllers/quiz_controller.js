e7035409f846adf73f65bcb3fa436f2a1b8f6ee3
johnnyc16 <johnnyc16@gmail.com>
2015-07-31 21:29:43 +0200

    busquedad

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,14 +12,19 @@ exports.load = function(req, res, next, quizId) {
   ).catch(function(error) { next(error);});
 };
 
-
 //GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index.ejs', { quizes: quizes});
+  if(req.query.search) {
+    var buscar = (req.query.search || '').replace(" ", "%").toLowerCase();
+    models.Quiz.findAll({where:["LOWER(pregunta) like ?", '%'+buscar+'%'],order:'pregunta ASC'}
+      ).then(function (quizes){
+        res.render('quizes/index', {quizes: quizes, errors: []});
+      }).catch(function (error) { next(error);});
+    } else {
+      models.Quiz.findAll().then(function (quizes){
+        res.render('quizes/index', {quizes: quizes, errors: []});
+      }).catch(function (error) { next(error);});
     }
-  ).catch(function(error) { next(error);})
 };
 
 //GET /quizes/:id
